9cc0d0d281f2f98d3d951f2d44792a015c94ba0a
ZhidkovMD <103329426+ZhidkovMD@users.noreply.github.com>
2024-05-18 14:39:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.07
+    alpha = 0.04
     z_a = stats.norm.ppf(1 - alpha)
     
     p1 = x_success / x_cnt # cont
