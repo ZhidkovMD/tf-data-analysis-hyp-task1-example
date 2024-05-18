2cc6c6cfccd6ce59105efec0344570a7321425e9
ZhidkovMD <103329426+ZhidkovMD@users.noreply.github.com>
2024-05-18 13:18:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,24 @@
-import pandas as pd
+import pandas as 
 import numpy as np
+import scipy.stats as stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 257689856 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    alpha = 0.07
+    z_a = stats.norm.ppf(1 - alpha)
+    
+    p1 = x_success / x_cnt # cont
+    p2 = y_success / y_cnt # test
+    p = (x_success + y_success) / (x_cnt + y_cnt) # all
+    
+    z = (p1 - p2) / np.sqrt(p * (1 - p) * (1 / x_cnt + 1 / y_cnt))
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return z > z_a # Ваш ответ, True или False
